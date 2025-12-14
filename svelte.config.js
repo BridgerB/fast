f0c6d8d1622bd8d7f4a85b89e86dc6c1f086ba7e
BridgerB <git@bridgerb.com>
2025-12-13 20:10:11 -0700

    Add Deno adapter for Deno Deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@deno/svelte-adapter";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = { kit: { adapter: adapter() } };
